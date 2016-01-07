4870abe0b197f7f294bf8c32df6b457879030fa2
Brandon Adler <Si-Down@users.noreply.github.com>
2016-01-07 11:36:56 -0500

    Create ScriptSigningProgram.ps1

---
+++ ScriptSigningProgram/ScriptSigningProgram.ps1
@@ -0,0 +1,7 @@
+$cert = (dir cert:currentuser\my\ -CodeSigningCert)
+
+$location = Read-Host "Where is your script that you would like signed located? Ex) C:\Users\brand\Desktop\MyScript.ps1"
+
+cd $location
+
+Set-AuthenticodeSignature $location $cert -TimestampServer http://timestamp.comodoca.com/authenticode
