41bfa394105f683e08c14e942c210690e8201f09
Brandon Adler <bcadler1@gmail.com>
2017-03-08 06:47:21 -0500

    Updated integrity checker for a path input and optional recursion.

--- integrityChecker.ps1
+++ integrityChecker.ps1
@@ -1,95 +1,151 @@
+<#
+ # 
+#>
+
 #Hashes items in a directory and stores it as a csv
 #if you run on a directory already hashed it will compare hashes
 
-$Path = "C:\Windows\System32\"
+param (
+    [Parameter(Mandatory=$true)]$Path,
+	[switch]$Recurse,
+    [string]$FileExtensions = "*"
+)
 
-$Provider = new-object System.Security.Cryptography.SHA1CryptoServiceProvider
-$Provider = new-object System.Security.Cryptography.MD5CryptoServiceProvider
-function get-filehash2($file, $type) {
+function Get-Files {
+	$FileExtensions = $FileExtensions.Split(',')
+
+	if ($Recurse -and $FileExtensions -eq "*") {
+		$files = Get-ChildItem -Path $Path -Recurse
+	}
+	elseif ($Recurse) {
+		foreach($ext in $FileExtensions) {
+			$files += Get-ChildItem -Path $Path -Recurse -Filter *.$ext
+		}
+	}
+	elseif (!$Recurse -and $FileExtensions -eq "*") {
+		$files = Get-ChildItem -Path $Path -Recurse
+	} 
+	else {
+		foreach ($ext in $FileExtensions) {
+			$files += Get-ChildItem -Path $Path -Recurse -Filter *.$ext
+		}
+	}
+	
+	return $files
+}
+
+function Get-FileHash($file, $type) {
+	$Provider = new-object System.Security.Cryptography.SHA1CryptoServiceProvider
+	$Provider = new-object System.Security.Cryptography.MD5CryptoServiceProvider
+
     if ($file -isnot [System.IO.FileInfo]) {
-      #write-error "'$($file)' is not a file."
-      return
+        #write-error "'$($file)' is not a file."
+        return
     }
     
     switch ($type) {
-    "MD5" {
-      $Provider = new-object System.Security.Cryptography.MD5CryptoServiceProvider
-      break
+        "MD5" {
+            $Provider = new-object System.Security.Cryptography.MD5CryptoServiceProvider
+            break
+        }
+        "SHA1" {
+            $Provider = new-object System.Security.Cryptography.SHA1CryptoServiceProvider
+            break
+        }
+        default {
+            throw "HashType must be one of the following: MD5 SHA1"
+        }
     }
-    "SHA1" {
-      $Provider = new-object System.Security.Cryptography.SHA1CryptoServiceProvider
-      break
-    }
-    default {
-      throw "HashType must be one of the following: MD5 SHA1"
-    }
-    }
-  
+      
     $hashstring = new-object System.Text.StringBuilder
     $stream = $file.OpenRead()
+
     if ($stream) {
-      foreach ($byte in $Provider.ComputeHash($stream)) {
-        [Void] $hashstring.Append($byte.ToString("X2"))
-      }
-      $stream.Close()
+        foreach ($byte in $Provider.ComputeHash($stream)) {
+            [Void]$hashstring.Append($byte.ToString("X2"))
+        }
+        $stream.Close()
     }
     $hashstring.ToString()
 }
-  
-$test = Test-Path -Path .\System32.csv
+
+$Path = $Path.Trim("`"")
+$folder = $Path.Split('\')
+$folder = $folder[$folder.Length-2]
+$csvPath = $PSScriptRoot + '\' + $folder + '.csv'
+Write-Host $csvPath
+
+$test = Test-Path -Path $csvPath
 if(!$test) {
     Write-Host Base Integrity Creation
     $results = @()
-    $files = Get-Childitem $Path -Filter *.exe
-    $files += Get-Childitem $Path -Filter *.dll
+
+	$files = Get-Files 
+	Write-Host "Out of Get-Files"
     foreach ($item in $files) {
         $fileObject = New-Object PSObject
     
         $fileObject | Add-Member -MemberType NoteProperty -Name "Filename" -Value $item."Name"
         $fileObject | Add-Member -MemberType NoteProperty -Name "LastWriteTime" -Value $item."LastWriteTime"
-        $md = get-filehash2 $item MD5
+        $md = Get-FileHash $item MD5
         $fileObject | Add-Member -MemberType NoteProperty -Name "MD5" -Value $md
-        $sha = get-filehash2 $item SHA1
+        $sha = Get-FileHash $item SHA1
         $fileObject | Add-Member -MemberType NoteProperty -Name "SHA1" -Value $sha
     
         $results += $fileObject
     }
 
-    $results | Export-csv System32.csv -notypeinformation
+    $results | Export-csv $csvPath -NoTypeInformation
+	Write-Host "Exiting Base Integrity"
     Exit
 } 
 else {
-Write-Host Integrity Check
-#Rename-Item .\System32.csv -NewName System32_Base.csv
-$files = Get-ChildItem $Path -Filter *.exe 
-$files += Get-ChildItem $Path -Filter *.dll 
-$results = @()
-foreach ($item in $files) {
-        $fileObject = New-Object PSObject
+    Write-Host Integrity Check
     
+    $files = Get-Files
+    $results = @()
+    foreach ($item in $files) {
+        $fileObject = New-Object PSObject
+
         $fileObject | Add-Member -MemberType NoteProperty -Name "Filename" -Value $item."Name"
         $fileObject | Add-Member -MemberType NoteProperty -Name "LastWriteTime" -Value $item."LastWriteTime"
-        $md = get-filehash2 $item MD5
+        $md = Get-FileHash $item MD5
         $fileObject | Add-Member -MemberType NoteProperty -Name "MD5" -Value $md
-        $sha = get-filehash2 $item SHA1
+        $sha = Get-FileHash $item SHA1
         $fileObject | Add-Member -MemberType NoteProperty -Name "SHA1" -Value $sha
-    
+
         $results += $fileObject
     }
 
-$results | Export-csv System32_New.csv -notypeinformation
+    $newCSVPath = $csvPath.Split('\')
+    $newCSVPath = $newCSVPath[$newCSVPath.Length-1]
+    $newCSVPath = $newCSVPath.Substring(0, $newCSVPath.IndexOf('.')) + "_new.csv"
+    $newCSVPath = $PSScriptRoot + '\' + $newCSVPath
+    $results | Export-csv -Path $newCSVPath -NoTypeInformation
 
-$oldCSV = Import-Csv .\System32.csv
-$oldCount = (Get-Content .\System32.csv -ReadCount 1 | Measure-Object -Line).Lines
-$newCSV = Import-Csv .\System32_New.csv
-$newCount = (Get-Content .\System32_New.csv -ReadCount 1 | Measure-Object -Line).Lines
-$diff = ($newCount - $oldCount)
-Write-Host Difference in num of files: $diff
+    $oldCSV = Import-Csv $csvPath
+    $oldCount = (Get-Content -Path $csvPath -ReadCount 1 | Measure-Object -Line).Lines
+    $newCSV = Import-Csv $newCSVPath
+    $newCount = (Get-Content -Path $newCSVPath -ReadCount 1 | Measure-Object -Line).Lines
+    $diff = ($newCount - $oldCount)
+    Write-Host Difference in num of files: $diff
 
-foreach ($item in $oldCSV) {
-    $newItem = $newCSV | where {$_."Filename" -eq $item."Filename"}
-    if($item."MD5" -ne $newItem."MD5") { Write-Host $item."Filename".ToString() MD5  Has been changed -ForegroundColor Red }
-    ElseIf ($item."SHA1" -ne $newItem."SHA1") { Write-Host $item."Filename" SHA1  Has been changed -ForegroundColor Red }
-    else {Write-Host $item."Filename"    Checked -ForegroundColor Green }
-}
+    foreach ($item in $oldCSV) {
+        $newItem = $newCSV | Where-Object {
+            $_."Filename" -eq $item."Filename"
+        }
+        if ($item."MD5" -ne $newItem."MD5") { 
+            Write-Host $item."Filename" MD5  Has been changed -ForegroundColor Red 
+        }
+        elseif ($item."SHA1" -ne $newItem."SHA1") { 
+            Write-Host $item."Filename" SHA1  Has been changed -ForegroundColor Red 
+        }
+        elseif ($item."LastWriteTime" -ne $newItem."LastWriteTime") {
+            Write-Host $item."LastWriteTime" Has been written to -ForegroundColor Red
+        }
+        else {
+            Write-Host $item."Filename"    Checked -ForegroundColor Green 
+        }
+    }
+    $results | Export-Csv -Path $csvPath -NoTypeInformation
 }
